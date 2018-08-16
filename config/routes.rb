a1539f111be12bd0ee87dfab6cdfb02700377f64
後藤悠伽 <haruka@gotouyuutoginoMacBook-Pro.local>
2018-08-17 08:41:25 +0900

    edit3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get "shops/index" => "shops#index"
-  get "shops/search" 
+  get "shops/search"
   get "shops/new" => "shops#new"
   get "shops/show" => "shops#show"
   get "shops/:id" => "shops#show"
   post "shops/:id/destroy" => "shops#destroy"
   devise_for :users
   resources :shops
+
+  authenticated :users do
+    root :to => "shops#index"
+  end
+  unauthenticated :users do
+    root :to => "home#index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
