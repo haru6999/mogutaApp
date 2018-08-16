96f56ad5ab170a170738d9a0cdde6b06bf8cf4af
後藤悠伽 <haruka@gotouyuutoginoMacBook-Pro.local>
2018-08-17 06:48:44 +0900

    add2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,15 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery #ない人追加
-//= require serviceworker-companion #追加
+//= require jquery
+//= require serviceworker-companion
 
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
 //= require serviceworker-companion
 
-// safari起動させないための記述
 $(function(){
   $('a').click(function(){
     location.href = $(this).attr('href');
